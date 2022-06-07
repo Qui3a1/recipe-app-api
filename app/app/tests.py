62b6877944b18c48ec4acb7f3f647545b1a2537b
Saruul <saruul-erdene@tussolution.mn>
2022-06-07 14:59:41 +0800

    fixing

--- app/app/tests.py
+++ app/app/tests.py
@@ -8,6 +8,7 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     """Test the calc module."""
+
     def tets_add_number(self):
         """test adding numbers together"""
         res = calc.add(5, 6)
